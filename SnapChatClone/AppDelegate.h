2be2ae4329d40ba815502fe552192e0fde04e709
Susan <susancodes@gmail.com>
2015-09-30 22:48:20 -0700

    Initial Commit

---
+++ SnapChatClone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnapChatClone
+//
+//  Created by Susan Chin on 9/30/15.
+//  Copyright © 2015 Susan Chin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
